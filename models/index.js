3ee29ac25adb74d21d7da5668e00363925f7976d
Shawn Analla <shawnanalla@gmail.com>
2022-02-04 17:46:26 -0800

    models created but reference nw

--- models/index.js
+++ models/index.js
@@ -5,12 +5,17 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_tag',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  foreignKey: 'product_tag',
+});
 
 module.exports = {
   Product,
